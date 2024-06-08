fcd9093019d048d520719ff9561e2908cbc24646
enomonn <enomonn123@gmail.com>
2024-06-09 00:11:42 +0900

    設定画面の設計

---
+++ database/migrations/2024_05_31_155601_create_goals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+           $table->id();
+            $table->string('goals_name', 50);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('start_day');
+            $table->foreignId('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+};
---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>   
+    <x-slot name="header">
+        Index
+   </x-slot>
+    </x-app-layout>
+        <h1>ホーム画面</h1>
+        <h2>一週間頑張ろう</h2>
+         </x-app-layout>
